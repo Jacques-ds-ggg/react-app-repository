0ce0117034e94f0a7cf3259b5a5b84633c69ac61
Jacques <jacquesds@tutanota.com>
2021-05-04 15:10:33 +0200

    Create a react app and publish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import fox from './noun_Fox_20234.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={fox} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Get all <code>code and files to </code>portfolio.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Look Up
         </a>
       </header>
     </div>
